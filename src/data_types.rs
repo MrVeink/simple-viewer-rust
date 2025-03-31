1eda0ebd48eae73ba7a0e1e25a7bfcc845adafcf
MrVeink <59528135+MrVeink@users.noreply.github.com>
2025-04-01 01:32:32 +0300

    Create data_types.rs

---
+++ src/data_types.rs
@@ -0,0 +1,22 @@
+use std::path::PathBuf;
+
+#[derive(Debug, Clone)]
+pub enum DataSource {
+    Local(PathBuf),
+    Cloud(String, String),  // (url, sheet_name)
+}
+
+#[derive(Debug, Clone)]
+pub struct TableData {
+    pub headers: Vec<String>,
+    pub rows: Vec<Vec<String>>,
+}
+
+impl TableData {
+    pub fn empty() -> Self {
+        TableData {
+            headers: Vec::new(),
+            rows: Vec::new(),
+        }
+    }
+}
